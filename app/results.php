6b3dd511e7c2719eac2b76100eaac4e3e6a21074
Simon Thulbourn <simon+github@thulbourn.com>
2012-03-14 22:52:32 +0000

    adds results page...

---
+++ app/results.php
@@ -0,0 +1,33 @@
+<?php
+	require 'BBCCoinAlgorithm.php';
+	
+	$amount = (isset($_POST['amount'])) ? $_POST['amount'] : 0;
+	
+	$coinAlgo = new BBCCoinAlgorithm();
+	
+	// get coins
+	$coins = $coinAlgo->getMinimumCoins($amount);
+	
+	if (count($coins) == 0) {
+		// invalid input
+		header("HTTP/1.1 400 Bad Request");
+		
+		$output = json_encode(array(
+			'status' => 'error',
+			'message' => 'Unable to process the input'
+		));
+	} else {
+		// all ok
+		header("HTTP/1.1 200 OK");
+		
+		$output = json_encode(array(
+			'status' => 'ok',
+			'coins' => $coins
+		));
+	}
+	
+	// set content-type to JSON
+	header("Content-Type: application/json;charset=utf-8");
+	
+	echo $output;
+?>
